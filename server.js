1df57297227347882d955081d75b719f18749b91
mcastillo40 <chgreen08@gmail.com>
2017-09-16 00:57:22 -0700

    added eventBrite js

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.set('port', 3000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use(express.static('public'));
 
 var meetupKeyID = '3b5cd41e32702b161a85e68646bb';  // Meet up API Key
