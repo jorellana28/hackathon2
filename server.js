7c80f049b42ee01b66d0c91cdcef4fef5134e9df
mcastillo40 <chgreen08@gmail.com>
2017-09-16 10:06:03 -0700

    Correct search for event city and remove duplicate js file

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
-});
+}); 
 
 app.use(express.static('public'));
 
