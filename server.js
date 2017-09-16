3f1a432d49477579d7e3000f8ed79750bb2bea80
jcisneros01 <cisnejos@oregonstate.edu>
2017-09-15 17:39:28 -0700

    changed port #

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3087);
+app.set('port', 3000);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
